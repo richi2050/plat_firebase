646f5c61cd6ea9a1b36a0e7439803c9c30b549d1
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-12-10 13:43:38 -0600

    se agregaron los scripts  de firebase y firebase auth

---
+++ public/js/config/ConfigFirebase.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyAAXfWNwnnXIwkCMEvqZSMcwcEW6i9cNDc",
+    authDomain: "plat-geekblog.firebaseapp.com",
+    databaseURL: "https://plat-geekblog.firebaseio.com",
+    projectId: "plat-geekblog",
+    storageBucket: "plat-geekblog.appspot.com",
+    messagingSenderId: "853596913256"
+  };
